fef4bf8f45fecf6f214fb7a7a2860a69419e5982
Inna Kukla <0969125133@ukr.net>
2023-04-08 03:19:45 +0300

    Create Home.styled.js

---
+++ src/components/pages/Home.styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const HomePageGreeting = styled.h1`
+  font-size: 60px;
+  line-height: 1.62;
+  letter-spacing: 0.06em;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 30px;
+  margin-top: 100px;
+  font-size: 70px;
+  font-weight: 700;
+  padding-bottom: 10px;
+
+  color: transparent;
+
+  -webkit-background-clip: text;
+  text-align: center;
+  font-weight: 600;
+  text-transform: uppercase;
+  font-style: italic;
+  font-family: 'Josefin Sans', sans-serif;
+  text-shadow: 2px 5px 0 rgba(0, 0, 0, 0.2);
+  background-color: #ff3cac;
+  background-image: linear-gradient(
+    225deg,
+    #ff3cac 0%,
+    #784ba0 50%,
+    #2b86c5 100%
+  );
+`;
