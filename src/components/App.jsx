7ca5f97c6730f3f022a0e7569ff6114089d72214
Inna Kukla <0969125133@ukr.net>
2023-04-08 03:18:59 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { AppBar } from './Header/AppBar';
+import Home from './pages/Home';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
+// import { Contacts } from './pages/Contacts';
+import { fetchCurrentUser } from 'redux/auth/operations';
+import { PrivateRoute } from './Header/PrivateRoute';
+import { Contacts } from './pages/Contacts';
+import { PublicRoute } from './Header/PublicRoute';
+
+export function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <AppBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+
+        <Route element={<PublicRoute />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/contacts" element={<Contacts />} />
+        </Route>
+      </Routes>
     </div>
   );
-};
+}
