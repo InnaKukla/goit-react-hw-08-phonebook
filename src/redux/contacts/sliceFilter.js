0643d91b57e0b80c68ecee1ceee8a05ccea83437
Inna Kukla <0969125133@ukr.net>
2023-04-08 03:20:11 +0300

    Create sliceFilter.js

---
+++ src/redux/contacts/sliceFilter.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+const filtersSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterContacts(state, action) {
+      return (state = action.payload);
+    },
+  },
+});
+
+export const { filterContacts } = filtersSlice.actions;
+export const filtersReducer = filtersSlice.reducer;
